e0c74ff5c2f8d1f22be3e1af67863f106eef5376
Josh Winter <winterjoshr@gmail.com>
2017-11-08 20:55:24 -0500

    fix def file generation in winbuild.bat

--- winbuild.bat
+++ winbuild.bat
@@ -7,6 +7,8 @@ echo BUILDING THE CLIENT
 cd client
 cl /EHsc /std:c++17 *.cc ..\sqlite3.lib ws2_32.lib /link /dll /out:chat.dll
 dumpbin /exports chat.dll > chat.exports
+echo LIBRARY CHAT > chat.def
+echo EXPORTS >> chat.def
 for /f "skip=19 tokens=4" %%A in (chat.exports) do echo %%A >> chat.def
 lib /def:chat.def /out:chat.lib /machine:x64
 del chat.exports
